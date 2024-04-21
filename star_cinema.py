43dd1e7792ae89ecd4db7807caa3f7c52217f4c5
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-04-21 23:13:18 +0530

    Added a class of Star_Cinema will be will inherited by Hall class

--- star_cinema.py
+++ star_cinema.py
@@ -1,1 +1,10 @@
-# will implement the class
+class Star_Cinema:
+    __hall_list = []
+    
+    @classmethod
+    def entry_hall(cls, hall):
+        cls.__hall_list.append(hall)
+    
+    @classmethod
+    def get_hall_list(cls):
+        return cls.__hall_list
